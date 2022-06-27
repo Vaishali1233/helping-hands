a69ac4fdf77446ce90bd2b8931222ba496dc3cce
Vaishali1233 <86144277+Vaishali1233@users.noreply.github.com>
2022-06-28 01:23:15 +0530

    Add files via upload

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import DynamicPosts from "../DynamicPosts/DynamicPosts";
+
+import "./Banner.css";
+
+function Banner() {
+  let [category, setCategory] = useState();
+  
+  return (
+    <div className="bannerParentDiv">
+      <div className="bannerChildDiv">
+        <div className="banner">
+          <img src="../../../Images/banner copy.png" alt="" />
+        </div>
+      </div>
+     { category!=null && <DynamicPosts category={category}/>  }
+    </div>
+  );
+}
+
+export default Banner;
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footerParentDiv">
+      <div className="footer">
+        <p>Free Classifieds in India. ©2022 Helping Hands</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
